86ed086a351b66cc1557040d3b84457400b8382d
Poli Petrova <62856922+PoliPetrova@users.noreply.github.com>
2022-09-07 13:59:34 +0300

    Add files via upload
    article entity, articleDto, articleService, articleRepo, articleController added.

---
+++ service/ArticleService.java
@@ -0,0 +1,49 @@
+package com.app.sportshubportal.service;
+
+import com.app.sportshubportal.entity.Article;
+import com.app.sportshubportal.exception.ArticleNotFoundException;
+import com.app.sportshubportal.repository.ArticleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ArticleService {
+
+    ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public Article addArticle(Article article) {
+        return this.articleRepository.save(article);
+    }
+
+
+    public Article updateArticle(Article article) {
+       return this.articleRepository.save(article);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Article> getAllArticles() {
+        return this.articleRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Article getArticleById(Long id) {
+        return articleRepository.findById(id).orElseThrow(() ->
+                new ArticleNotFoundException("Article with id: " + id + " was not found."));
+    }
+
+    public void removeArticle(Long id) {
+         this.articleRepository.deleteById(id);
+    }
+
+}
+
+
+
